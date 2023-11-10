4c436082963b450645ee98c3cb182e9601d02070
lbarg <lbambrosi@gmail.com>
2023-11-09 18:34:28 -0600

    adding Practica 6

---
+++ Practica6/tbl_emp_details.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `tbl_emp_details` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+  `department` varchar(255) NOT NULL,
+  `uname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY(`id`)
+);
