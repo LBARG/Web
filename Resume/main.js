ad86bd98a07bcfef5e416e85573f652550de7bcc
lbarg <lbambrosi@gmail.com>
2023-11-09 18:27:03 -0600

    second commit

---
+++ Resume/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",
+{
+    strings:["Engineering Student","Problem Solver", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDeleay:1000,
+    loop:true
+})
